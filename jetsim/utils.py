e1bfbcfc8c431d7bc3de86af7294d74df58cffd0
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-03-02 19:24:31 +0300

    calc direction in function itself

--- jetsim/utils.py
+++ jetsim/utils.py
@@ -31,7 +31,7 @@ def luminosity_distance(z, H_0=73.0, omega_M=0.3, omega_V=0.7, format="cm"):
         raise Exception('Format=\"cm\" or \"Mpc\"')
 
 
-def transfer_stokes(stokes1, v1, v2, n1, n2, bf2):
+def transfer_stokes(stokes1, v1, v2, n1, bf2):
     """
     Transfer stokes vector from frame that has velocity v1 in observer frame to
     frame that has velocity v2 in observer frame. Index 2 means value in second
@@ -45,8 +45,6 @@ def transfer_stokes(stokes1, v1, v2, n1, n2, bf2):
         Velocity of second frame relative to observer frame.
     :param n1:
         Direction of propagation in first RF.
-    :param n2:
-        Direction of propagation in second RF.
     :param bf2:
         Direction of B-field in second RF.
     :return:
@@ -55,7 +53,11 @@ def transfer_stokes(stokes1, v1, v2, n1, n2, bf2):
     # Find Doppler factor of v2 relative to v1
     v2r1 = velsum(v2, -v1)
     G2r1 = 1. / math.sqrt(1. - v2r1.dot(v2r1))
+    # Direction of propagation in second RF.
+    n2 =  (n1 + G2r1 * v2r1 * (G2r1 * n1.dot(v2r1) / (G2r1 + 1.) - 1.)) / \
+          (G2r1 * (1. - n1.dot(v2r1)))
     D2r1 = 1. / (G2r1 * (1. - n2.dot(v2r1)))
+
     I1, Q1, U1, V1 = stokes1
     LP1 = math.sqrt(Q1 ** 2. + U1 ** 2.)
     chi1 = math.atan2(U1, Q1)
@@ -72,4 +74,5 @@ def transfer_stokes(stokes1, v1, v2, n1, n2, bf2):
     chi2 = math.acos(((bf2 - bf2.dot(n2) * n2) / np.linalg.norm(bf2 - bf2.dot(n2) * n2)) * e2 / np.linalg.norm(e2))
     Q2 = LP2 * math.cos(2. * chi2)
     U2 = LP2 * math.sin(2. * chi2)
+
     return np.array([I2, Q2, U2, V2])
