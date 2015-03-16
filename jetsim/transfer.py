013f78cf956e368d6faed47b6a157d5fda47d4b8
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-03-16 21:03:39 +0300

    now pixsize in mas

--- jetsim/transfer.py
+++ jetsim/transfer.py
@@ -4,6 +4,7 @@ import numpy as np
 from multiprocessing import Pool
 from geometry import Ray
 from jet import Jet
+from utils import mas_to_pc
 
 
 def unwrap_self_transfer(arg, **kwargs):
@@ -13,12 +14,34 @@ def unwrap_self_transfer(arg, **kwargs):
 # TODO: I need more fine grid close to BH.
 # TODO: Keep redshift and cosmological stuff here?
 class Transfer(object):
-    def __init__(self, jet, imsize, los_angle, pixsize=(1., 1.,)):
+    """
+    Class that describes transfer of radiation through jet.
+
+    :param jet:
+        Instance of ``Jet`` class.
+    :param imsize:
+        Tuple of image sizes for each of 2 map dimensions (int x int).
+    :param los_angle:
+        L.O.S. angle.
+    :param pixsize:
+        Tuple of pixel sizes in mas for each of 2 map dimensions.
+    :param z: (optional)
+        Cosmological redshift.
+    :param nu_obs: (optional)
+        Observing frequency [GHz].
+    """
+    def __init__(self, jet, los_angle, imsize, pixsize, z=0.5, nu_obs=5.):
         self.jet = jet
+        self.jet.set_obs_frequency(nu_obs)
+        self.jet.set_redshift(z)
+        self.los_angle = los_angle
         self.los_direction = (math.sin(los_angle), 0, -math.cos(los_angle))
-        self.yscale = pixsize[0]
-        self.zscale = pixsize[1]
+        self.yscale = pixsize[0] * mas_to_pc(z)
+        self.zscale = pixsize[1] * mas_to_pc(z)
+        self.pixsize = pixsize
         self.imsize = imsize
+        self.z = z
+        self.nu_obs = nu_obs
         self.image = {'I': np.zeros(imsize), 'Q': np.zeros(imsize),
                       'U': np.zeros(imsize), 'V': np.zeros(imsize)}
         # Arrays of coordinates
@@ -30,8 +53,8 @@ class Transfer(object):
         image_coordinates = np.dstack((y, z))
         # Coordinates in jet frame
         jet_coordinates = image_coordinates.copy()
-        jet_coordinates[..., 0] = image_coordinates[..., 0] * pixsize[0]
-        jet_coordinates[..., 1] = image_coordinates[..., 1] * pixsize[1] /\
+        jet_coordinates[..., 0] = image_coordinates[..., 0] * self.yscale
+        jet_coordinates[..., 1] = image_coordinates[..., 1] * self.zscale /\
             math.sin(self.jet.geometry.angle)
         # Add zero x-coordinate for points in (yz)-jet plane
         jet_coordinates = np.dstack((np.zeros(imsize), jet_coordinates))
@@ -93,8 +116,9 @@ class Transfer(object):
 
 if __name__ == '__main__':
 
-    jet = Jet(nu=5., z=0.5)
-    transfer = Transfer(jet, (50, 50,), math.pi/6., pixsize=(0.05, 0.05,))
+    jet = Jet()
+    transfer = Transfer(jet, los_angle=0.1, imsize=(100, 100,),
+                        pixsize=(0.0005, 0.0005,), z=0.1, nu_obs=5.)
     t1 = time.time()
     transfer.transfer(n=50)
     # result = transfer.transfer_mp()
