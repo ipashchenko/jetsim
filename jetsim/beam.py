ea32e8eff427a7cd1dfeb3d1891c3e67132abb1e
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-03-20 12:01:39 +0300

    refactor

--- jetsim/beam.py
+++ jetsim/beam.py
@@ -1,52 +1,61 @@
+import math
+import numpy as np
 from scipy import signal
-from data_io import get_fits_image_info
-from utils import gaussianBeam
 
 
-# * Resolution could depend on position in map or frequency. Should i associate
-# instance of ``Beam`` with each pixel in ``Image`` instance in such cases?
-# * Beam is connected with ``CCModel`` instance naturally by deconvolution
-# process? NO! Delta functions can be used in modelling without CLEAN!
-def create_clean_beam_from_fits(fname):
+def gaussianBeam(size_x, bmaj, bmin, bpa, size_y=None):
     """
-    Create instance of ``CleanBeam`` from FITS-file of CLEAN image.
-    """
-    imsize, pixref, (bmaj, bmin, bpa), pixsize = get_fits_image_info(fname)
-    return CleanBeam(bmaj, bmin, bpa, imsize)
+    Generate and return a 2D Gaussian function
+    of dimensions (size_x,size_y).
 
+    See Briggs PhD (Appendix B) for details.
 
-class Beam(object):
+    :param size_x:
+        Size of first dimension [pixels].
+    :param bmaj:
+        Beam major axis size [pixels].
+    :param bmin:
+        Beam minor axis size [pixels].
+    :param bpa:
+        Beam positional angle [deg].
+    :param size_y (optional):
+        Size of second dimension. Default is ``size_x``.
+    :return:
+        Numpy.ndarray of size (``size_x``, ``size_y``,).
     """
-    Basic class that represents point spread function.
-    """
-    def __init__(self):
-        self.image = None
+    size_y = size_y or size_x
+    x, y = np.mgrid[-size_x: size_x + 1, -size_y: size_y + 1]
+    # Constructing parameters of gaussian from ``bmaj``, ``bmin``, ``bpa``.
+    a0 = 1. / (0.5 * bmaj) ** 2.
+    c0 = 1. / (0.5 * bmin) ** 2.
+    theta = math.pi * (bpa + 90.) / 180.
+    a = math.log(2) * (a0 * math.cos(theta) ** 2. +
+                       c0 * math.sin(theta) ** 2.)
+    b = (-(c0 - a0) * math.sin(2. * theta)) * math.log(2.)
+    c = math.log(2) * (a0 * math.sin(theta) ** 2. +
+                       c0 * math.cos(theta) ** 2.)
 
-    # Convolve with any object that has ``image`` attribute
-    def convolve(self, image_like):
-        return signal.fftconvolve(self.image, image_like.image, mode='same')
-
-
-class DirtyBeam(Beam):
-    """
-    Class that represents point spread function.
-    """
-    def fit_central_part(self):
-        """
-        Fit central part of dirty beam and return instance of ``CleanBeam``.
-        """
-        pass
+    g = np.exp(-a * x ** 2. - b * x * y - c * y ** 2.)
+    # FIXME: It is already normalized?
+    # return g/g.sum()
+    return g
 
 
 # TODO: bmaj & bmin must be in pixels!!!
-class CleanBeam(Beam):
+class Beam(object):
     """
     Class that represents central part of point spread function.
     """
     def __init__(self, bmaj=None, bmin=None, bpa=None, size=None):
+        # Number of pixels for bmaj & bmin
         self.bmaj = bmaj
         self.bmin = bmin
         self.bpa = bpa
+        # Size of map to convolve with
         self.size = size
         self.image = gaussianBeam(self.size[0], self.bmaj, self.bmin, self.bpa,
                                   self.size[1])
+
+    # Convolve with any object that has ``image`` attribute
+    def convolve(self, image_like):
+        return signal.fftconvolve(self.image, image_like, mode='same')
