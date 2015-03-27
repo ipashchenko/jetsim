1cefcf19ef2c86411b19105060f2f4f52970b3a2
Ilya Pashchenko <in4pashchenko@gmail.com>
2015-03-27 13:24:37 +0300

    +vec functions

--- jetsim/vfields.py
+++ jetsim/vfields.py
@@ -13,6 +13,15 @@ class VField(object):
         """
         raise NotImplementedError
 
+    def v_vec(self, ps):
+        """
+        Velocity field of jet in observer rest frame in triangular coordinates.
+        :param ps:
+            Numpy array with shape (N, 3,) of rectangular coordinates. N -
+            number of points
+        """
+        raise NotImplementedError
+
 
 class FlatVField(VField):
     """
@@ -25,6 +34,12 @@ class FlatVField(VField):
 
     def v(self, x, y, z):
         return  np.array([0., 0., np.sign(z) * self.v_z])
+
+    def v_vec(self, ps):
+        vx = np.zeros(len(ps), dtype=float)
+        vy = np.zeros(len(ps), dtype=float)
+        vz = np.sign(ps[:, 2]) * self.v_z
+        return  np.asarray((vx, vy, vz,)).T
 
 
 class CentralVField(VField):
@@ -40,3 +55,16 @@ class CentralVField(VField):
         return  np.array([v_r * x / np.sqrt(x * x + y * y + z * z),
                           v_r * y / np.sqrt(x * x + y * y + z * z),
                           v_r * z / np.sqrt(x * x + y * y + z * z)])
+
+    def v_vec(self, ps):
+        # r-component of velocity in spherical coordinates
+        v_r = math.sqrt(1. - 1. / self.gamma0 ** 2.)
+        xs = ps[:, 0]
+        ys = ps[:, 1]
+        zs = ps[:, 2]
+
+        vx = v_r * xs / np.sqrt(xs * xs + ys * ys + zs * zs)
+        vy = v_r * ys / np.sqrt(xs * xs + ys * ys + zs * zs)
+        vz = v_r * zs / np.sqrt(xs * xs + ys * ys + zs * zs)
+
+        return np.asarray((vx, vy, vz,)).T
